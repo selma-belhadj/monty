af8ec15ea87f6e97130d95ec07c80c22982646cf
selma <belhadjselma1@gmail.com>
2022-12-15 22:29:09 +0100

    Implemented the mod opcode that computes the rest of the division of the second top element of the stack by the top element of the stack

--- calculate.c
+++ calculate.c
@@ -71,3 +71,24 @@ void _mul(stack_t **h, unsigned int line_number)
 	free((*h)->prev);
 	(*h)->prev = NULL;
 }
+/**
+ * _mod - mod second node from top node
+ * @h: top of list
+ * @line_number: line of command
+ */
+void _mod(stack_t **h, unsigned int line_number)
+{
+	if (h == NULL || *h == NULL)
+	{
+		printf("L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*h)->n == 0 || (*h)->next->n == 0)
+	{
+		printf("L%u: division by zero\n", line_number);
+	}
+	(*h)->next->n %= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
+}
