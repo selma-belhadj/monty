3bec123f151bcad52544de1f5ecfe044e63b40ec
selma <belhadjselma1@gmail.com>
2022-12-15 22:27:13 +0100

    Implemented the div opcode that divides the second top element of the stack by the top element of the stack

--- calculate.c
+++ calculate.c
@@ -33,3 +33,24 @@ void _sub(stack_t **h, unsigned int line_number)
 	free((*h)->prev);
 	(*h)->prev = NULL;
 }
+/**
+ * _div - divide second node from top node
+ * @h: top of list
+ * @line_number: line of command
+ */
+void _div(stack_t **h, unsigned int line_number)
+{
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*h)->n == 0 || (*h)->next->n == 0)
+	{
+		printf("L%u: division by zero\n", line_number);
+	}
+	(*h)->next->n /= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
+}
