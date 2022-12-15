1ca149e4eb126952f277709f89be44b548b3a256
selma <belhadjselma1@gmail.com>
2022-12-15 22:37:44 +0100

    Implemented the rotl opcode that rotates the stack to the top

--- position.c
+++ position.c
@@ -29,3 +29,26 @@ void swap(stack_t **h, unsigned int line_number)
 	tmp->next = *h;
 	(*h) = tmp;
 }
+/**
+ * rotl - rotate so top of stack becomes last one, second becomes first one
+ * @h: node to be rotated
+ * @line_number: node number
+ */
+void rotl(stack_t **h, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void) line_number;
+
+	if ((*h)->next != NULL)
+	{
+		tmp = *h;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		(*h)->prev = tmp;
+		tmp->next = *h;
+		(*h)->next->prev = NULL;
+		*h = (*h)->next;
+		tmp->next->next = NULL;
+	}
+}
