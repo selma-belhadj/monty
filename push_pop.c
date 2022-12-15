26e1f83727413bbc29a27dd8eef38396eddf090c
selma <belhadjselma1@gmail.com>
2022-12-15 09:11:32 +0100

    Imlemented the push function that adds an element to the top of the stack

--- push_pop.c
+++ push_pop.c
@@ -18,3 +18,29 @@ int is_number(const char *n)
 	}
 	return (0);
 }
+
+/**
+ * push - adds node to the start of dlinkedlist
+ * @h: head of linked list (node at the bottom of stack)
+ * @line_number: bytecode line number
+ * @n: integer
+ */
+void push(stack_t **h, unsigned int line_number, const char *n)
+{
+	if (!h)
+		return;
+	if (is_number(n) == -1)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		free_dlist(h);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (add_end_node(h, atoi(n)) == -1)
+		{
+			free_dlist(h);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
