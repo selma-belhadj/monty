b0082e3d397f08bba46542a295296b835b011717
selma <belhadjselma1@gmail.com>
2022-12-15 22:28:18 +0100

    Implemented the mul opcode that multiplies the second top element of the stack with the top element of the stack

--- calculate.c
+++ calculate.c
@@ -54,3 +54,20 @@ void _div(stack_t **h, unsigned int line_number)
 	free((*h)->prev);
 	(*h)->prev = NULL;
 }
+/**
+ * _mul - multiply second node and top node
+ * @h: top of list
+ * @line_number: line of command
+ */
+void _mul(stack_t **h, unsigned int line_number)
+{
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n *= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
+}
