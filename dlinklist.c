955fbf567c8d6fbd1304fdf3b04eca7badbacaf1
selma <belhadjselma1@gmail.com>
2022-12-15 11:10:21 +0100

    Implemented the delete_end_node function that deletes node at end of doubly linked list

--- dlinklist.c
+++ dlinklist.c
@@ -36,3 +36,26 @@ int add_end_node(stack_t **h, int n)
 	}
 	return (0);
 }
+
+/**
+ * delete_end_node - deletes node at end of doubly linked list
+ * @h: pointer to head of doubly linked list
+ */
+void delete_end_node(stack_t **h)
+{
+	stack_t *del = NULL;
+
+	/* account for only one node in list */
+	del = *h;
+	if ((*h)->next == NULL)
+	{
+		*h = NULL;
+		free(del);
+	}
+	else /* else delete front, and link correctly */
+	{
+		*h = (*h)->next;
+		(*h)->prev = NULL;
+		free(del);
+	}
+}
