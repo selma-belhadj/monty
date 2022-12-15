7a4cecc39a88ffbde56266cf8e0c7e1a202e986c
selma <belhadjselma1@gmail.com>
2022-12-15 22:26:16 +0100

    Implemented the sub opcode that subtracts the top element of the stack from the second top element of the stack

--- calculate.c
+++ calculate.c
@@ -16,3 +16,20 @@ void _add(stack_t **h, unsigned int line_number)
 	free((*h)->prev);
 	(*h)->prev = NULL;
 }
+/**
+ * _sub - subtract second node from top node
+ * @h: top of list
+ * @line_number: line of command
+ */
+void _sub(stack_t **h, unsigned int line_number)
+{
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*h)->next->n -= (*h)->n;
+	(*h) = (*h)->next;
+	free((*h)->prev);
+	(*h)->prev = NULL;
+}
