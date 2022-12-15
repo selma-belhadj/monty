78ce7668996f8c072340a27bb9245159a6e36b80
selma <belhadjselma1@gmail.com>
2022-12-15 22:38:39 +0100

    Implemented the rotr opcode that rotates the stack to the bottom

--- position.c
+++ position.c
@@ -52,3 +52,26 @@ void rotl(stack_t **h, unsigned int line_number)
 		tmp->next->next = NULL;
 	}
 }
+/**
+ * rotr - rotate so only bottom node of stack becomes first one
+ * @h: node to be rotated
+ * @line_number: node number
+ */
+void rotr(stack_t **h, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	(void) line_number;
+
+	if ((*h)->next != NULL)
+	{
+		tmp = *h;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		(*h)->prev = tmp;
+		tmp->next = *h;
+		tmp->prev->next = NULL;
+		tmp->prev = NULL;
+		(*h) = (*h)->prev;
+	}
+}
