43b95fef725430d0adc10e4f4fcc7430cbecbab4
selma <belhadjselma1@gmail.com>
2022-12-15 11:14:13 +0100

    Implemented the free_dlist that frees a doubly linked list with only int data, no strings

--- dlinklist.c
+++ dlinklist.c
@@ -59,3 +59,21 @@ void delete_end_node(stack_t **h)
 		free(del);
 	}
 }
+
+/**
+ * free_dlist - frees a doubly linked list with only int data, no strings
+ * @h: pointer to head of list
+ */
+void free_dlist(stack_t **h)
+{
+	/* return if empty list */
+	if (!h)
+		return;
+
+	while (*h && (*h)->next)
+	{
+		*h = (*h)->next;
+		free((*h)->prev);
+	}
+	free(*h);
+}
