da9862ba516c1e426f6eacf761b34782e2f5b556
selma <belhadjselma1@gmail.com>
2022-12-15 22:30:54 +0100

    Implemented the pchar opcode that prints the char at the top of the stack, followed by a new line

--- prints_opcodes.c
+++ prints_opcodes.c
@@ -45,3 +45,24 @@ void nop(stack_t **h, unsigned int line_number)
 	(void) h;
 	(void) line_number;
 }
+/**
+ * pchar - print top node in stack as ascii letter
+ * @h: head of list
+ * @line_number: bytecode line number
+ */
+void pchar(stack_t **h, unsigned int line_number)
+{
+	if (!h || !*h)
+	{
+		printf("L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (((*h)->n) >= 0 && ((*h)->n) <= 127)
+		printf("%c\n", (*h)->n);
+	else
+	{
+		printf("L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+}
